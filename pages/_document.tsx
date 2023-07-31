2be8fe2c2cab06ca896b1b596cbda7567831eb1c
Francois Mechin <f.mechin@protonmail.com>
2023-08-01 01:41:57 +0200

    small fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { useIntl } from "react-intl";
 
 export default function Document() {
-	const intl = useIntl();
-	const title = intl.formatMessage({ id: "page.home.head.title" });
-	const description = intl.formatMessage({ id: "page.home.head.meta.desc" });
-	const metaImg = "../public/assets/fmlogo-FULL-BG-colors.png";
-
 	return (
 		<Html lang="en">
 			<Head />
