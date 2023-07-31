4e5abb60c4712e3fc9667866c2b48a330694ad96
Francois Mechin <f.mechin@protonmail.com>
2023-07-31 21:58:58 +0200

    Fixed some bugs

--- pages/index.jsx
+++ pages/index.jsx
@@ -24,21 +24,11 @@ export default function Home() {
 				<title>{title}</title>
 				<meta name="description" content={description}/>
 				<meta name="viewport" content="width=device-width, initial-scale=1"/>
-				<link rel="icon" href="/favicon.ico" hrefLang="x-default"/>
-				<link rel="icon" href="/favicon.ico" hrefLang="en"/>
-				<link rel="icon" href="/favicon.ico" hrefLang="fr"/>
+				<link rel="icon" href="https://cdn.icon-icons.com/icons2/1451/PNG/512/codefolder_99364.png" hrefLang="x-default"/>
+				<link rel="icon" href="https://cdn.icon-icons.com/icons2/1451/PNG/512/codefolder_99364.png" hrefLang="en"/>
+				<link rel="icon" href="https://cdn.icon-icons.com/icons2/1451/PNG/512/codefolder_99364.png" hrefLang="fr"/>
 			</Head>
 			<Main />
-			<div>
-				<h1>
-					<FormattedMessage id="page.home.title" values={{ b: (info) => <p>{ info }</p> }}/>
-					<br />
-					{description}
-				</h1>
-				<p>
-					<FormattedMessage id="page.home.description" />
-				</p>
-			</div>
 			<About />
 			<Skills />
 			<Projects />
