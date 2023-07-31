3b7be98091f3b2502e5e3c434e5c52654fb25313
Francois Mechin <f.mechin@protonmail.com>
2023-08-01 01:38:06 +0200

    Small fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,48 +9,7 @@ export default function Document() {
 
 	return (
 		<Html lang="en">
-			<Head>
-				<meta name="description" content={description} />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-
-				{/* <!-- Schema.org markup for Google+ --> */}
-				<meta itemProp="name" content={title} />
-				<meta itemProp="description" content={description} />
-				<meta itemProp="image" content={metaImg} />
-				{/* <!-- Open Graph data --> */}
-
-				<meta property="og:title" content={title} />
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://francoismechin.fr" />
-				<meta property="og:image" content={metaImg} />
-				<meta property="og:description" content={description} />
-				<meta property="og:site_name" content={title} />
-
-				{/* <!-- Twitter Card data --> */}
-				<meta name="twitter:card" content="summary_large_image" />
-				<meta name="twitter:site" content="@publisher_handle" />
-				<meta name="twitter:title" content={title} />
-				<meta name="twitter:description" content={description} />
-				<meta name="twitter:creator" content="@author_handle" />
-				{/* <-- Twitter Summary card images must be at least 200x200px --> */}
-				<meta name="twitter:image" content={metaImg} />
-
-				<link
-					rel="icon"
-					href="https://cdn.icon-icons.com/icons2/1451/PNG/512/codefolder_99364.png"
-					hrefLang="x-default"
-				/>
-				<link
-					rel="icon"
-					href="https://cdn.icon-icons.com/icons2/1451/PNG/512/codefolder_99364.png"
-					hrefLang="en"
-				/>
-				<link
-					rel="icon"
-					href="https://cdn.icon-icons.com/icons2/1451/PNG/512/codefolder_99364.png"
-					hrefLang="fr"
-				/>
-			</Head>
+			<Head />
 			<body>
 				<Main />
 				<NextScript />
